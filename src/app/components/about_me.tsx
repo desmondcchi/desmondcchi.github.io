7b2d9e35b25c60be7d869ef38e8d69c4bb10f321
Desmond Chi <desmond.chi.g@gmail.com>
2024-01-18 09:17:49 -0800

    Fix error with `About Me` component.

---
+++ src/app/components/about_me.tsx
@@ -0,0 +1,11 @@
+export default function AboutMe() {
+  return (
+    <div className="about_me flex mt-96">
+      <h1>About Me</h1>
+      <p>
+        <b>Hi!</b> I am currently a junior studying CS @ UC Riverside. My
+        aspiration is to become a software engineer.
+      </p>
+    </div>
+  );
+}
