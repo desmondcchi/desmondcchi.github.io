3a7e7b29a6c54b34ab02c1a42e73bb2bf2955720
Desmond Chi <desmond.chi.g@gmail.com>
2024-10-04 03:02:08 -0700

    [Test] Trigger workflow.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       </div>
       <AboutMe />
       <Experience />
-      <p>Test GitHub actions.</p>
+      <p>Test GitHub actions 2.</p>
     </main>
   );
 }
