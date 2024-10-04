dda045e8db123f4340fecbf705d3405cfaec7fd6
Desmond Chi <desmond.chi.g@gmail.com>
2024-10-04 02:14:19 -0700

    Add `key` prop to `ExperienceEntry` to fix error.

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -13,8 +13,8 @@ export default function Experience() {
     <div className="my-[500px] flex items-center justify-center text-left">
       <div className="w-[1000px]">
         <b className="heading_text text-7xl leading-[10rem]">Experience</b>
-        {experiences.map((experience) => (
-          <ExperienceEntry props={experience} />
+        {experiences.map((experience, i) => (
+          <ExperienceEntry key={i} props={experience} />
         ))}
       </div>
     </div>
