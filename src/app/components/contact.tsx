2f4beed1c78eebbd1474382ef55adb1911cb1742
Desmond Chi <desmond.chi.g@gmail.com>
2024-10-16 15:26:13 -0700

    Add Resume and hover animations for `Contact Me` section.

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 
 export default function Contact() {
   return (
@@ -10,25 +11,48 @@ export default function Contact() {
           <b className="text-5xl">Contact Me</b>
         </div>
 
-        <div className="flex items-center justify-center">
-          <FontAwesomeIcon icon={faEnvelope} className="w-8 m-5" />
-          <p>desmonddchi@gmail.com</p>
+        <div className="flex items-start justify-center">
+          <div className="flex items-center justify-center font-thin">
+            <FontAwesomeIcon icon={faEnvelope} className="w-8 m-5" />
+            <p>desmonddchi@gmail.com</p>
+          </div>
         </div>
 
-        <div className="flex items-center justify-center m-[-2rem]">
+        <div className="flex items-start justify-center m-[-1.25rem]">
+          <a
+            href="./data/Desmond%20Chi%20Resume.pdf"
+            download="Desmond Chi Resume.pdf"
+          >
+            <div className="flex items-center justify-center font-thin ml-[-8rem]">
+              <FontAwesomeIcon
+                icon={faFile}
+                className="w-8 m-5"
+              ></FontAwesomeIcon>
+              <p className="hover:text-blue-400">Resume</p>
+            </div>
+          </a>
+        </div>
+
+        <div className="flex items-center justify-center m-[1.5rem]">
           <a
             className="m-7"
             href="https://github.com/desmondcchi"
             target="_blank"
           >
-            <FontAwesomeIcon icon={faGithub} className="w-20" />
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="w-20 hover:text-slate-400 transition-all duration-300 ease-in-out"
+            />
           </a>
           <a
             className="m-7"
             href="https://www.linkedin.com/in/desmondchi/"
             target="_blank"
           >
-            <FontAwesomeIcon icon={faLinkedin} className="w-20" />
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="w-20 hover:text-slate-400 transition-all duration-300 ease-in-out"
+            />
           </a>
         </div>
       </div>
